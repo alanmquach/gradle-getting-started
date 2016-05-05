c9841336235b72a904c87f9d85946f9e68939c04
Alan Quach <aquach@netflix.com>
2016-05-04 23:19:14 -0700

    Added comments and heroku PORT binding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,14 @@ import static spark.Spark.*;
  
 public class Main {
     public static void main(String[] args) {
+        // Heroku's convention is to define the port that you should listen on under the environment variable PORT
+        // Here, we read that port, then tell Spark to bind to that port.
+        port(Integer.valueOf(System.getenv("PORT")));
+
+        // Default route
         get("/", (request, response) -> "Hello, World!");
+
+        // Parameterized route
         get("/:name", (request, response) -> {
             String name = request.params(":name");
             return "Hello: " + request.params(":name"); 
